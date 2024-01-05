e378267431328e77643ddf4e70176d41dc77bc13
Koyuki Takeda <koyuki.t@KoyukinoMacBook-Pro-2.local>
2024-01-05 09:52:21 +0900

    header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -16,53 +16,30 @@ const Header = (props: HeaderProps) => {
   const { logo, logoLink, links } = props;
 
   return (
-    <header className="Header relative">
-      <div className="container py-5 flex justify-start md:justify-between">
+    <header className="relative" style={{ height: '80px', overflow: 'hidden' }}>
+      <div className="container py-5 flex justify-start md:justify-between" style={{ width: '1170px' }}>
+        <div>
+          <p style={{
+            fontSize: '13px',
+            lineHeight: '23px',
+            letterSpacing: '0.1em',
+            fontWeight: '400',
+            marginTop: '15px',
+            color: '#808080'
+          }}>
+            ブランド品の高価買取なら<br />創業76年上場企業のKOMEHYOへ
+          </p>
+        </div>
+
         {logo && (
-          <MaybeLink className="Header-logoLink" href={logoLink}>
+          <a href={logoLink}>
             <div className="flex w-[144px] mr-2">
               <Image image={logo} layout="fill" />
             </div>
-          </MaybeLink>
+          </a>
         )}
 
-        <div className="hidden md:flex items-center">
-          <ul className="flex">
-            {links.map((item: CTA) => (
-              <li key={item.label}>
-                <Link
-                  className="Link Link--primary Link--header mx-2 lg:mx-5"
-                  cta={item}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <button
-          className="flex md:hidden absolute p-4 right-0 top-1/2 -translate-y-1/2"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? <FaTimes /> : <FaBars />}
-          <span className="sr-only">Toggle Header Menu</span>
-        </button>
-      </div>
-
-      <div
-        className={
-          c({ visible: menuOpen }) +
-          "hidden absolute top-full left-0 right-0 h-screen bg-white"
-        }
-      >
-        <div className="container">
-          <ul className="flex flex-col">
-            {links.map((item: CTA) => (
-              <li key={item.label}>
-                <Link className="Link Link--header py-3 block" cta={item} />
-              </li>
-            ))}
-          </ul>
-        </div>
+        {/* ここに追加のナビゲーションリンクや他の要素を追加 */}
       </div>
     </header>
   );
